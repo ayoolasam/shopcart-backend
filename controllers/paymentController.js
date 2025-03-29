9cc502aab8ef15f43a8203f544bc63c0eb4b16cf
ayoolasam <obayomisamuel941@gmail.com>
2025-03-29 10:18:22 +0100

    fixes

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -3,24 +3,19 @@ const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const Order = require("../models/order");
 
 exports.webHook = catchAsyncErrors(async (req, res, next) => {
-const event = req.body;
+  const event = req.body;
 
+  if (event.event === "charge.success") {
+    const reference = event.data.reference;
 
-if(event.event === "charge.success"){
-  const reference = event.data.reference;
+    const order = await Order.findOne({ reference: reference });
+    if (!order) {
+      return next(new errorHandler("Order not found", 404));
+    }
 
+    order.paymentInfo.paymentStatus = "Successfull";
 
-const order = await Order.findOne({reference: reference});
-if(!order){
-  return next(new errorHandler("Order not found", 404));
-}
-
-
-order.paymentInfo.paymentStatus = "Successfull"
-
-await order.save();
-res.status(`200`)
-
-}
-
+    await order.save();
+    res.status(200);
+  }
 });
